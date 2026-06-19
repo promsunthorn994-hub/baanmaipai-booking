e8537bbe2298c146cc5fd60597c14fd1176c73b2
promsunthorn994-hub <promsunthorn994@gmail.com>
2026-06-19 19:01:58 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+[build]
+  command = "npm run build"
+  publish = "dist"
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-})
+[[redirects]]
+  from = "/*"
+  to = "/index.html"
+  status = 200
